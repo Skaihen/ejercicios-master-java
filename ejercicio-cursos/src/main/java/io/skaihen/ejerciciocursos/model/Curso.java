fae6fa280ee540878edc2e966e475166e7d4a95c
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-27 09:54:41 +0200

    database

--- ejercicio-cursos/src/main/java/io/skaihen/ejerciciocursos/model/Curso.java
+++ ejercicio-cursos/src/main/java/io/skaihen/ejerciciocursos/model/Curso.java
@@ -1,5 +1,8 @@
 package io.skaihen.ejerciciocursos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "cursos")
 public class Curso {
+    @Id
     private int codigo;
     private String nombre;
     private int duracion;
