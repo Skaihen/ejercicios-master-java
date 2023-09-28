6fcf135e58bb2cf74d0a44a98ffce1afa146bead
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-28 10:39:01 +0200

    ejercicio videoclub

--- ejercicio-cursos/src/main/java/io/skaihen/ejerciciocursos/repository/CursosRepository.java
+++ ejercicio-cursos/src/main/java/io/skaihen/ejerciciocursos/repository/CursosRepository.java
@@ -11,5 +11,4 @@ public interface CursosRepository extends JpaRepository<Curso, Integer> {
 
     @Query("SELECT c FROM Curso c WHERE c.precio BETWEEN :precioMin AND :precioMax")
     List<Curso> findAllBetweenPrices(int precioMin, int precioMax);
-
 }
