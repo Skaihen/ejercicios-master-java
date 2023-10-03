3e0c5a6aa46a0ae7ec8443b1fd32adac5d8c8ac3
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-03 15:13:44 +0200

    Squashed commit of the following:
    commit 8993e1752a3a660e72c7ea256a6b58546ba41183
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Tue Oct 3 14:03:44 2023 +0200
    
        javadocs
    
    commit fc19e644d6d7c56c9120e86f8552b8a736810a70
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Tue Oct 3 11:34:47 2023 +0200
    
        libros finished
    
    commit 5d4beb235cae171c9911881e5c750a498aae409a
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Tue Oct 3 11:09:22 2023 +0200
    
        finished
    
    commit d0bc7b3c489061a0b2c9d275a2dee67b8746bbc9
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Tue Oct 3 10:46:24 2023 +0200
    
        ejercicio2 working
    
    commit a2c9a09428a161b646f786a793e9c2454ac002b4
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Mon Oct 2 17:58:23 2023 +0200
    
        falta acabar ejercicio2
    
    commit bfbe96aa08f1f58dadd7483ae6158e2645ee3412
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Mon Oct 2 17:54:39 2023 +0200
    
        ejercicio2
    
    commit 18d5678b4d7813b6c9bd8b8942c0af235d7979c5
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Mon Oct 2 17:40:00 2023 +0200
    
        ejercicio crud
    
    commit 75e6e10e543121053abe79c13ae9f47f45ea9d3f
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Mon Oct 2 15:15:44 2023 +0200
    
        Create settings.json
    
    commit 91a0328729444673bbde1fed2dd2ac46c7ec828b
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Fri Sep 29 17:57:03 2023 +0200
    
        consumer finished
    
    commit bce5941d0c534f17964a0513f8a4f3442875db65
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Fri Sep 29 17:42:19 2023 +0200
    
        backend
    
    commit 725d85963be6ee1c0d4cf420efb7cfbcc9f0ee18
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Fri Sep 29 10:50:10 2023 +0200
    
        biblioteque
    
    commit a61123af5afa77b73a9c36f90345be31a847b7ee
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Thu Sep 28 12:54:41 2023 +0200
    
        all queries
    
    commit 6824f470f17fa95421752c2f880f63b6876c4097
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Thu Sep 28 12:43:39 2023 +0200
    
        peliculas JDBC
    
    commit 6fcf135e58bb2cf74d0a44a98ffce1afa146bead
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Thu Sep 28 10:39:01 2023 +0200
    
        ejercicio videoclub
    
    commit a74cdb8899ce0494b97749663816ed36afbc7eeb
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Wed Sep 27 17:18:57 2023 +0200
    
        Create JPA-JDBC-Hibernate.md
    
    commit c4995174b65cce110647d161da2f099ed8409286
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Wed Sep 27 15:50:28 2023 +0200
    
        readme
    
    commit 3f955df8356027f3e3725a96efa0f1a29a38e80a
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Wed Sep 27 13:51:48 2023 +0200
    
        100% working
    
    commit 3c38fcc2a657122546125e9728c62a73c0e5e594
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Wed Sep 27 13:37:21 2023 +0200
    
        everythings works
    
    commit 188cc76df0926a81a2ee994d8cf4a30766bff640
    Author: Skaihen <92516247+Skaihen@users.noreply.github.com>
    Date:   Wed Sep 27 12:09:20 2023 +0200
    
        working more or less

--- ejercicio-desarrollo-microservicio-crud-cursos/ejercicio1/src/main/java/io/skaihen/ejercicio1/repositories/CursoRepository.java
+++ ejercicio-desarrollo-microservicio-crud-cursos/ejercicio1/src/main/java/io/skaihen/ejercicio1/repositories/CursoRepository.java
@@ -11,6 +11,16 @@ import io.skaihen.ejercicio1.models.Curso;
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Integer> {
 
+    /**
+     * This function retrieves a list of courses whose price falls within a
+     * specified range.
+     *
+     * @param priceMin The minimum price value for the courses.
+     * @param priceMax The maximum price value for the courses.
+     * @return The method `findAllBetweenPriceMinMax` returns a list of `Curso`
+     *         objects that have a price
+     *         between the `priceMin` and `priceMax` values.
+     */
     @Query("SELECT c FROM Curso c WHERE c.precio BETWEEN :priceMin AND :priceMax")
     List<Curso> findAllBetweenPriceMinMax(double priceMin, double priceMax);
 }
